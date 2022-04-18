834afa26220d5e973f15b08dc070c40d190f7654
Semaf Electronics <semaf@users.noreply.github.com>
2022-04-18 23:54:38 +0200

    Add files via upload

---
+++ before-hooks.d/00-example.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This file must be executable, to make it as such, run the following:
+#   chmod u+x <this-file>
+# It can also be written in any language so long as a valid shebang (#!) is used as above
