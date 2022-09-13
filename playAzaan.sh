4bcc24c31552c69c8b98b402f7017ac3a4c32495
Semaf Electronics <semaf@users.noreply.github.com>
2022-09-13 09:08:12 +0200

    general update
    set volume, set mp3 file path with curl

--- playAzaan.sh
+++ playAzaan.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 if [ $# -lt 1 ]; then
-  echo "USAGE: $0 <azaan-audio-path> [<volume>]"
+  echo "USAGE: $0 [<volume> 1-100] <azaan-audio-path>"
   exit 1
 fi
 
-audio_path="$1"
-vol=${2:-0}
+vol="$1"
+audio_path="$2"
 root_dir=`dirname $0`
+JSONRPC="http://localhost:9000/jsonrpc.js"
+UUID="aa:bb:cc:dd:ee"
 
 # Run before hooks
 for hook in $root_dir/before-hooks.d/*; do
-    echo "Running before hook: $hook"
+    #echo "Running before hook: $hook"
     $hook
 done
 
 # Play Azaan audio
 #omxplayer --vol $vol -o local $audio_path
-curl -d '{"id":0,"params":["aa:aa:00:00:00:00",["mixer","volume",$vol]],"method":"slim.request"}' http://localhost:9000/jsonrpc.js
-curl -d '{"id":0,"params":["aa:aa:00:00:00:00",["playlist","play","$audio_path"]],"method":"slim.request"}' http://localhost:9000/jsonrpc.js
+curl -i -X POST -H "Content-Type: application/json" -d '{"id":1,"params":["'$UUID'",["mixer","volume","'$vol'"]],"method":"slim.request"}' $JSONRPC
+curl -i -X POST -H "Content-Type: application/json" -d '{"id":1,"method":"slim.request","params":[ "'$UUID'", ["playlist", "play", "'$audio_path'"] ]}' $JSONRPC
+#curl -i -X POST -H "Content-Type: application/json" -d '{"id":1,"method":"slim.request","params":[ "'$UUID'", ["'status'","-",1] ]}' $JSONRPC
 
 
-
-# Run after hooks
+ Run after hooks
 for hook in $root_dir/after-hooks.d/*; do
     echo "Running after hook: $hook"
     $hook
